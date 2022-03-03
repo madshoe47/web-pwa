2b7c87f53cfef30a5430807c80f2d64969dad49f
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-03-04 00:22:07 +0100

    L6: Basic Service Wookiee site

---
+++ lesson-06/service-wookiee/main.js
@@ -0,0 +1,48 @@
+const template = document.querySelector("#template").content;
+const boxes = document.querySelector("#boxes");
+
+for (const fileType of ["text/css", "text/html", "application/json"]) {
+  // Clone the template for each box
+  const box = template.cloneNode(true);
+
+  // Request fake.css, fake.html and fake.json, respectively
+  const fileName = "fake." + fileType.split("/")[1];
+
+  box.querySelector("button").textContent = "Fetch " + fileName;
+
+  // Send fetch request on button click
+  box.querySelector("button").addEventListener("click", async (event) => {
+    try {
+      const response = await fetch(fileName, {
+        headers: {
+          Accept: fileType,
+        },
+      });
+
+      // Show response headers
+      const headersList = event.target.parentNode.querySelector("dl");
+      headersList.innerHTML = "";
+      for (const [key, value] of response.headers.entries()) {
+        headersList.insertAdjacentHTML(
+          "beforeend",
+          `<dt class="capitalize font-bold">${key}:</dt><dd>${value}</dd>`
+        );
+      }
+
+      // Show response content
+      const content = await response.text();
+      event.target.parentNode.querySelector("textarea").value = content.trim();
+
+      // Throw an error if the response is not "ok" (i.e. not 200-299 range)
+      if (!response.ok) {
+        throw new Error(response.status + " " + response.statusText);
+      }
+    } catch (error) {
+      // Change button to display error status
+      event.target.classList.remove("bg-yellow-800");
+      event.target.classList.add("bg-red-700");
+      event.target.textContent = error.message;
+    }
+  });
+  boxes.appendChild(box);
+}
