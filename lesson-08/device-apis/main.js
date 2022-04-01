2ddc404670bb1ae6d9beb9f579956863d169327f
madshoe47 <56107464+madshoe47@users.noreply.github.com>
2022-04-01 10:23:14 +0200

    geolocation

--- lesson-08/device-apis/main.js
+++ lesson-08/device-apis/main.js
@@ -1,5 +1,19 @@
 for (const button of document.querySelectorAll("button[id]")) {
   button.addEventListener("click", () => {
-    alert("Not implemented yet.");
+    if ("geolocation" in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition((position) => {
+        let positionLatitude = position.coords.latitude;
+        let positionLongitude = position.coords.longitude;
+        console.log(positionLatitude + ", " + positionLongitude);
+
+        document.getElementById(
+          "geolocation"
+        ).innerHTML = `${positionLatitude}, ${positionLongitude}`;
+      });
+    } else {
+      /* geolocation IS NOT available */
+      console.log("Geolocatation isn't available");
+    }
   });
 }
